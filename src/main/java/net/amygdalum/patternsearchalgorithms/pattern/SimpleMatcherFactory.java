bfa18654bff98022cc5b3ba49292030510454558
almondtools <mandelst@gmail.com>
2017-02-12 15:05:50 +0100

    simple matcher

--- src/main/java/net/amygdalum/patternsearchalgorithms/pattern/SimpleMatcherFactory.java
+++ src/main/java/net/amygdalum/patternsearchalgorithms/pattern/SimpleMatcherFactory.java
@@ -1,9 +1,5 @@
 package net.amygdalum.patternsearchalgorithms.pattern;
 
-import static java.lang.Character.MAX_VALUE;
-import static java.lang.Character.MIN_VALUE;
-import static java.util.Arrays.asList;
-
 import net.amygdalum.patternsearchalgorithms.dfa.DFA;
 import net.amygdalum.patternsearchalgorithms.nfa.NFA;
 import net.amygdalum.patternsearchalgorithms.nfa.NFABuilder;
@@ -14,38 +10,48 @@ public class SimpleMatcherFactory implements MatcherFactory {
 
 	private SearchMode mode;
 	private DFA matcher;
-	private NFA finder;
+	private NFA grouper;
 
-	public SimpleMatcherFactory(SearchMode mode, DFA matcher, NFA finder) {
+	public SimpleMatcherFactory(SearchMode mode, DFA matcher, NFA grouper) {
 		this.mode = mode;
 		this.matcher = matcher;
-		this.finder = finder;
+		this.grouper = grouper;
 	}
 
 	public static SimpleMatcherFactory compile(NFA nfa, SearchMode mode) {
-		return new SimpleMatcherFactory(mode, matcherFrom(nfa), finderFrom(nfa));
+		return new SimpleMatcherFactory(mode, matcherFrom(nfa), grouperFrom(nfa));
 	}
 
-	private static NFA finderFrom(NFA nfa) {
-		NFABuilder builder = new NFABuilder(nfa.getCharset());
-		
-		NFAComponent base = builder.matchGroup(nfa.clone().asComponent(), 0);
-		NFAComponent selfloop = builder.matchStarLoop(builder.match(MIN_VALUE, MAX_VALUE));
-		NFAComponent search = builder.matchConcatenation(asList(selfloop, base));
-		
-		NFA finder = search.toFullNFA(nfa.getCharset());
-		finder.prune();
-		
-		return finder;
-	}
-
-	private static DFA matcherFrom(NFA nfa) {
+	public static DFA matcherFrom(NFA nfa) {
 		return DFA.from(nfa);
+	}
+
+	private static NFA grouperFrom(NFA nfa) {
+		NFABuilder builder = new NFABuilder(nfa.getCharset());
+
+		NFAComponent base = builder.matchGroup(nfa.clone().asComponent(), 0);
+
+		NFA finder = base.toFullNFA(nfa.getCharset());
+		finder.prune();
+
+		return finder;
 	}
 
 	@Override
 	public Matcher newMatcher(ByteProvider input) {
-		return new SimpleMatcher(mode, matcher, finder, input);
+		if (mode.findLongest()) {
+			if (mode.findOverlapping()) {
+				return new SimpleLongestOverlappingMatcher(matcher, grouper, input);
+			} else {
+				return new SimpleLongestNonOverlappingMatcher(matcher, grouper, input);
+			}
+		} else {
+			if (mode.findOverlapping()) {
+				return new SimpleAllOverlappingMatcher(matcher, grouper, input);
+			} else {
+				return new SimpleAllNonOverlappingMatcher(matcher, grouper, input);
+			}
+		}
 	}
 
 }
